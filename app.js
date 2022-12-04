66f114db489be4454f31d5d27cad4d49ca14bfe6
james4-daf <d.james@elsevier.com>
2022-12-04 11:16:42 +0000

    some progress, not fully working at all

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use("/", index);
 const celebrityRoute = require("./routes/celebrities/celebrity.routes");
 app.use("/", celebrityRoute);
 
+const movieRoute = require("./routes/movies/movies.routes");
+app.use("/", movieRoute);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/celebrities/celebrity.routes.js
+++ routes/celebrities/celebrity.routes.js
@@ -2,13 +2,53 @@ const CelebrityModel = require("../../models/Celebrity.model");
 
 const router = require("express").Router();
 
-/* GET home page */
+/* GET celebrity page page */
 router.get("/celebrities", (req, res, next) => {
-  CelebrityModel.find().then((celebrities) => {
-    res.render("/celebrities/index.hbs", { celebrities }).catch((err) => {
+  CelebrityModel.find()
+    .then((celebrities) => {
+      res.render("celebrities/index.hbs", { celebrities });
+    })
+    .catch((err) => {
       console.log("Celebrities fetch failed", err);
     });
-  });
+});
+
+//get celebrity detail page
+router.get("/celebrities/:id", (req, res, next) => {
+  const { id } = req.params;
+  CelebrityModel.findById(id)
+    .then((celeb) => {
+      res.render("celebrities/show.hbs", { celeb });
+      console.log("Celebrity fetched", res);
+    })
+    .catch((err) => {
+      console.log("Celebrity fetch failed", err);
+    });
+});
+
+router.get("/celebrities/create", (req, res, next) => {
+  CelebrityModel.create()
+    .then((celebrities) => {
+      res.render("celebrities/new.hbs");
+      console.log("Celebrity fetched", res);
+    })
+    .catch((err) => {
+      console.log("Celebrity create failed", err);
+    });
+});
+
+router.post("/celebrities/create", (req, res, next) => {
+  // Iteration #3: Add a new drone
+
+  // ... your code here
+  const { name, occupation, catchPhrase } = req.body;
+  CelebrityModel.create({ name, occupation, catchPhrase })
+    .then(() => {
+      res.redirect("celebrities/index.hbs");
+    })
+    .catch((err) => {
+      console.log("celebrities creating failed", err);
+    });
 });
 
 module.exports = router;
