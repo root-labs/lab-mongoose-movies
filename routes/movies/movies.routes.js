c27b1ca818f57b36752155b28e8be33eb8ea70a6
james4-daf <d.james@elsevier.com>
2022-12-08 09:28:10 +0000

    like 90% done

--- routes/movies/movies.routes.js
+++ routes/movies/movies.routes.js
@@ -12,6 +12,7 @@ router.get("/movies", (req, res, next) => {
     });
 });
 
+//get req to get movies from db
 router.get("/movies/new", (req, res, next) => {
   CelebrityModel.find()
     .then((celebrities) => {
@@ -22,6 +23,7 @@ router.get("/movies/new", (req, res, next) => {
     });
 });
 
+//make new movie
 router.post("/movies/new", (req, res, next) => {
   const { title, genre, plot, cast } = req.body;
   MovieModel.create({ title, genre, plot, cast })
@@ -34,4 +36,56 @@ router.post("/movies/new", (req, res, next) => {
     });
 });
 
+//detail page
+router.get("/movie/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log("errrrrrrrrrrrr" + id);
+  MovieModel.findById(id)
+    .then((movie) => {
+      res.render("movies/show.hbs", { movie });
+      console.log("Movie fetched", res);
+    })
+    .catch((err) => {
+      console.log("Movie fetch failed", err);
+    });
+});
+
+//updating
+router.get("/movie/:id/edit", (req, res, next) => {
+  const { id } = req.params;
+  MovieModel.findById(id)
+    .then((movie) => {
+      res.render("movies/update.hbs", { movie });
+    })
+    .catch((err) => {
+      console.log("movie edit failed", err);
+    });
+});
+
+router.post("/movie/:id/edit", (req, res, next) => {
+  const { tile, genre, plot, cast } = req.body;
+  const { id } = req.params;
+  MovieModel.findByIdAndUpdate(id, { tile, genre, plot, cast })
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("movie update failed", err);
+    });
+});
+
+//delete request
+
+router.post("/movie/:id/delete", (req, res, next) => {
+  // Iteration #5: Delete the drone
+  // ... your code here
+  const { id } = req.params;
+  MovieModel.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("movie Delete failed", err);
+    });
+});
 module.exports = router;
